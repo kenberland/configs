88cee7c3b80e3542a47d639dadcd5258f0910b1c
Kenneth Berland <berlandk@amazon.com>
2018-10-06 15:49:12 -0700

    foo

--- bin/sea-hotel.sh
+++ bin/sea-hotel.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
 ID=eDP
 LAPTOP=$(xrandr | grep $ID |cut -d\  -f1)
-EXTERNAL=$(xrandr | grep \ connected | grep -v $ID | cut -d\  -f1)
+EXTERNAL=DP1
 xrandr --output $EXTERNAL --auto --output $LAPTOP --off
+xinput --set-prop 16 267 3
